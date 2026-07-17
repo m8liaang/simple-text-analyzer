3e5f4efec75161ba1a88f1f2924a461b5081d672
Michelle <michelle8.liang@gmail.com>
2026-07-17 18:53:02 +0400

    Update frequencies.py

--- src/frequencies.py
+++ src/frequencies.py
@@ -5,31 +5,25 @@ import pandas as pd
 def word_counter(txt):
   txt_words = {}
 
-  try:
-      with open(txt) as text:
-        text = text.read()
-        words = re.findall(r'\b\w+\b', text)
+  words = re.findall(r'\b\w+\b', txt)
 
-      for word in words:
-        # Lower case all the words
-        word = word.lower()
+  for word in words:
+    # Lower case all the words
+    word = word.lower()
 
-        # Count number of times the word appears
-        txt_words[word] = txt_words.get(word, 0) + 1
+    # Count number of times the word appears
+    txt_words[word] = txt_words.get(word, 0) + 1
         
-        # Convert dictionary into df (organized into a table, highest to lowest frequencies)
-      words_table = pd.DataFrame(txt_words.items(), columns=['Word', 'Frequency'])
-      words_table = words_table.sort_values(by='Frequency', ascending=False)
+  # Convert dictionary into df (organized into a table, highest to lowest frequencies)
+  words_table = pd.DataFrame(txt_words.items(), columns=['Word', 'Frequency'])
+  words_table = words_table.sort_values(by='Frequency', ascending=False)
         
-      pd.set_option('display.max_rows', None)
-      pd.set_option('display.max_columns', None)
-      pd.set_option('display.max_colwidth', None)
+  pd.set_option('display.max_rows', None)
+  pd.set_option('display.max_columns', None)
+  pd.set_option('display.max_colwidth', None)
 
-      return words_table
-  
-  except FileNotFoundError:
-    print("File not found.")
+  return words_table
 
-# Execute Function
+# Test Function
 if __name__ == "__main__":
   word_counter("demo.txt")
